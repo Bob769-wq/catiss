8b04b717804a7f79e8f82d499626600693a493cb
Bob <dash78231@gmail.com>
2025-09-11 21:02:59 +0800

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Crumb } from './crumb/crumb';
+import { HeroSection } from './hero-section/hero-section';
+import { Product } from './product/product';
+import { MiddleBanner } from './middle-banner/middle-banner';
+import { AboutUs } from './about/about';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Header, Crumb, HeroSection, Product, MiddleBanner, AboutUs, Footer],
+  template: `
+    <app-header />
+    <div class="big:max-w-[var(--container)] mx-auto">
+      <app-crumb />
+      <app-hero-section />
+      <app-product />
+    </div>
+    <app-middle-banner />
+    <div class="big:max-w-[var(--container)] px-[.9375rem] mx-auto">
+      <app-about />
+    </div>
+    <app-footer />
+
+    <div class="fixed right-5 bottom-5 z-50">
+      <button
+        class="w-16 h-16 bg-[#3C5997] rounded-full rounded-ee-none flex justify-center items-center"
+      >
+        <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <circle cx="3" cy="12" r="3" />
+          <circle cx="12" cy="12" r="3" />
+          <circle cx="21" cy="12" r="3" />
+        </svg>
+      </button>
+    </div>
+  `,
+  styles: ``,
 })
-export class App {
-  protected readonly title = signal('catiss');
-}
+export class App {}
